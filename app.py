1545ff579a3e472ee258403787a99e9477c1d76f
infirid <inforiddhyahealthcare@gmail.com>
2025-07-07 14:06:18 +0530

    app.py

--- app.py
+++ app.py
@@ -35,5 +35,8 @@ def generate_pdf():
     pdf_file.seek(0)
     return send_file(pdf_file, as_attachment=True, download_name='application_form.pdf', mimetype='application/pdf')
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
